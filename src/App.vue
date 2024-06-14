8f46b05d509075cc87215deac0884d153aef9219
wadias98 <wadiaz@uce.edu.ec>
2024-06-13 19:38:48 -0500

    prueba

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="score">
+    <p>Puntaje: {{ puntaje }}</p>
+    <p>Intento: {{ intento }}</p>
+  </div>
+  <div class="hijo">
+    <Hijo :texto="dato1" :img="imagen1" />
+    <Hijo :texto="dato2" :img="imagen2" />
+    <Hijo :texto="dato3" :img="imagen3" />
+  </div>
+
+  <button @click="jugar()">JUGAR</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Hijo from "@/components/Hijo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Hijo,
+  },
+  data() {
+    return {
+      imagen1: "https://via.placeholder.com/250",
+      imagen2: "https://via.placeholder.com/250",
+      imagen3: "https://via.placeholder.com/250",
+      dato1: "xxxxxxxxxxx",
+      dato2: "xxxxxxxxxxx",
+      dato3: "xxxxxxxxxxx",
+      puntaje: 0,
+      intento: 0,
+      names: [],
+      images:[],
+      pokemon:[],
+    };
+  },
+  methods: {
+    getRandomInt(max) {
+      return Math.floor(Math.random() * max);
+    },
+
+    async jugar() {
+      
+      await this.obtenerPokemon();
+      
+    },
+    async obtenerPokemon() {
+      var aux =[];
+        for (let i = 0; i <= 5; i++) {
+          aux.push
+          const {name} = await fetch(`https://pokeapi.co/api/v2/pokemon/${i+1}`).then(r => r.json());
+          
+          console.log(name)
+          this.names.push(name)
+         this.images.push(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i+1}.svg`)
+         console.log(this.images[i])
+      }
+      
+       this.imagen1 =  this.images[this.getRandomInt(5)];
+      this.imagen2 =  this.images[this.getRandomInt(5)];
+      this.imagen3 =  this.images[this.getRandomInt(5)];
+      this.dato1 = this.names[this.getRandomInt(5)];
+      this.dato2 = this.names[this.getRandomInt(5)];
+      this.dato3 = this.names[this.getRandomInt(5)];      
+    },
+    verificar(e) {
+      
+    }
+  },
+};
 </script>
 
 <style>
@@ -22,5 +79,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.score {
+  display: grid;
+  grid-template-columns: repeat(2, 550px);
+  justify-content: center;
+}
+.hijo {
+  display: grid;
+  grid-template-columns: repeat(3, 450px);
 }
 </style>
---
+++ src/components/Hijo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div class="hijo">
+      <img :src="img" alt="" />
+    </div>
+    <p>{{ texto }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    img: {
+      type: String,
+      required: true,
+
+    },
+    texto: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style >
+img {
+  width: 250px;
+  height: 250px;
+}
+
+</style>
